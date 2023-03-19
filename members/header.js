46ce5f510572d40088226ec2b71e2d547d5e7fc5
llzhaoshuo <l_zhaoshuo@163.com>
2023-03-19 21:17:48 +0800

    codes update

--- members/header.js
+++ members/header.js
@@ -6,6 +6,7 @@ document.writeln("   				<li><a href=\"../../publications.html\">论文专利</a
 document.writeln("   				<li><a href=\"../../project.html\">科研项目</a></li>")
 document.writeln("   				<li><a href=\"../../awards.html\">获奖信息</a></li>")
 document.writeln("   				<li><a href=\"../../books.html\">学术专著与教材</a></li>")
+document.writeln("   				<li><a href=\"../../codes.html\">开源代码</a></li>")
 document.writeln("   				<li><a href=\"../../member.html\">学生信息</a></li>")
 document.writeln("   				<li><a href=\"../../recruit.html\">招生信息</a></li>")
 document.writeln("    			</ul>")
