760c017d11b28412e54a43393f1467feb04448a1
llzhaoshuo <l_zhaoshuo@163.com>
2023-03-23 17:21:04 +0800

    add visit

--- footer.js
+++ footer.js
@@ -1,4 +1,5 @@
 document.writeln("    <div class=\"footer-content\">")
+document.writeln("		<p white-space=\"nowrap\">访问次数：<a href=\"http://cn.web-counter.net\" title=\"计数器\"><img src=\"http://www.web-counter.net/count_20091204.php?c=U3hgQbpAOtT\"  alt=\"计数器\" /></a></p>")
 document.writeln("    	<p><a href=\"http://www.neu.edu.cn\">东北大学 计算机科学与工程学院 人工智能系<a></p>")
 document.writeln("    <p style=\"font-size:14px\">Copyright 2023 DFLAG-NEU. All rights reserved.</p>")
 document.writeln("    </br>")
